ce8036ae88dcd8f8c399841cae2fc5e1e17e59ac
yyi <yyi.mailer@gmail.com>
2018-09-04 21:40:20 +0800

    get file from http request

--- src/test/java/cn/lechange/happor/controllers/TestNormalHandler.java
+++ src/test/java/cn/lechange/happor/controllers/TestNormalHandler.java
@@ -4,17 +4,31 @@ import cn.lechange.happor.annotation.Controller;
 import cn.lechange.happor.controller.HttpNormalHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
+import io.netty.handler.codec.http.multipart.InterfaceHttpData;
 
-@Controller(method="GET", uriPattern="^/test/(\\w+)")
+@Controller(method="POST", uriPattern="^/test/(\\w+)")
 public class TestNormalHandler extends HttpNormalHandler {
 	
 	@Override
 	protected void handle(FullHttpRequest request, FullHttpResponse response) {
 		// TODO Auto-generated method stub
+		HttpPostRequestDecoder httpPostRequestDecoder = new HttpPostRequestDecoder(request);
+		while(httpPostRequestDecoder.hasNext()) {
+			InterfaceHttpData data = httpPostRequestDecoder.next();
+			System.out.println(getUploadFileName(data));
+		}
 		String words = "hello world " + getUriParser().getSection(1);
 		response.content().writeBytes(words.getBytes());
 		response.headers().set("Content-Type", "text/plain");
 		response.headers().set("Content-Length", response.content().readableBytes());
+	}
+
+	private String getUploadFileName(InterfaceHttpData data) {
+		String content = data.toString();
+		String temp = content.substring(0, content.indexOf("\n"));
+		content = temp.substring(temp.lastIndexOf("=") + 2, temp.lastIndexOf("\""));
+		return content;
 	}
 
 	@Override
